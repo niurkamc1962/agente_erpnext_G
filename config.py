e883ecde35b5dabff11e1e2eb6cccdfda4b25924
Niurka <niurkamc1962@gmail.com>
2026-03-30 15:14:59 -0400

    Modificado el PATH

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# config.py
 import os
 from dotenv import load_dotenv
 from pathlib import Path
@@ -6,13 +7,16 @@ load_dotenv()
 
 
 class Config:
+    # Esta línea detecta automáticamente la carpeta donde está config.py
     BASE_DIR = Path(__file__).resolve().parent
-    BENCH_PATH = Path(os.getenv("BENCH_PATH", "./apps"))
-    # Asegúrate de tener esta carpeta creada o cambia la ruta
-    DOCS_PATH = Path(os.getenv("DOCS_PATH", "./documentacion_local"))
 
-    MODEL = os.getenv("OLLAMA_MODEL", "qwen2.5-7b-instruct-q4_k_m")
-    # Usaremos el mismo modelo para embeddings o uno ligero como 'nomic-embed-text'
+    # Unimos la base con la ruta del .env para tener una ruta absoluta dinámica
+    # Si en el .env dice "info_erp/apps", se convertirá en "/ruta/actual/del/proyecto/info_erp/apps"
+    BENCH_PATH = BASE_DIR / os.getenv("BENCH_PATH", "info_erp/apps")
+    DOCS_PATH = BASE_DIR / os.getenv("DOCS_PATH", "info_erp/documentacion")
+
+    MODEL = os.getenv("OLLAMA_MODEL", "qwen2.5-1.5b-instruct-q4_k_m.0:latest")
     EMBED_MODEL = "nomic-embed-text"
 
+    # La base de datos vectorial también relativa a la raíz
     STORAGE_DIR = str(BASE_DIR / "storage_vectorial")
